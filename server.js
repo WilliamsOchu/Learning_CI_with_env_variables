de9a15ce40191ef5620c8694f35e684bdb60f209
unplugged001 <ochuwilliamsirawo@gmail.com>
2022-06-23 19:21:39 +0100

    tweaked the server.js and added new build command in travis file

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const DOCKER_USERNAME = process.env.DOCKER_USERNAME;
+const DOCKER_PASSWORD = process.env.DOCKER_PASSWORD;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -5,17 +7,9 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    console.log(`USERNAME is ${DOCKER_USERNAME}`);
+    console.log(`My password is ${DOCKER_PASSWORD}`);
     await sleep(5000);
   }
 }
-
-
-const DOCKER_USERNAME = process.env.DOCKER_USERNAME;
-console.log(`USERNAME is ${DOCKER_USERNAME}`);
-
-
-const DOCKER_PASSWORD = process.env.DOCKER_PASSWORD;
-console.log(`My password is ${DOCKER_PASSWORD}`);
-
 main();
